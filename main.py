099d7f3bd7a7a371f63b417b21b8b7ef309abf58
Philipp Sontag <philso705@gmail.com>
2025-09-25 17:51:27 +0200

    Added .gitignore file and updated README.md, main.py, requirements.txt, and userInterface.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from model import ngramModel
-from model import ngramModel
 from userInterface import userInterface
 
 if __name__ == "__main__":
--- userInterface.py
+++ userInterface.py
@@ -8,7 +8,7 @@ class userInterface:
 
     def startInterface(self,model=None):
         while True:
-            inputInt = int(input("[1] Load training Data\n[2] Generate Text\n[3] Save the model (Currently requires large amount of RAM with larger models)\n[4] Load a model\n[5] Add dataset\n[6] Exit\n1-6: "))
+            inputInt = int(input("[1] Load training Data\n[2] Generate Text\n[3] Save model (Currently requires large amount of RAM with larger models)\n[4] Load model\n[5] Add dataset\n[6] Exit\n1-6: "))
 
             if inputInt == 1:
                 try:
@@ -30,19 +30,11 @@ class userInterface:
                 #    print("File not found")
             elif inputInt == 2:
                 if model != None:
-                    inputInt = int(input("[1] Generate sentences with context\n[2] Generate sentences without context\n1-2: "))
-                    if inputInt == 1:
-                        contextString = input("Preceeding text: ")
-                        textSize = int(input("Sentence amount: "))
-                        print(model.generateAmountOfSentences(textSeed=contextString,sentNum=textSize))
-                    elif inputInt == 2:
-                        try:
-                            wordNum = int(input("Limit the length of the sentence (will stop if '.' is reached): "))
-                            print(model.generateSentence(wordNum=wordNum))
-                        except ValueError:
-                            print("Invalid value for length of the sentence")
-                    else:
-                        print("Invalid input")
+                    contextString = input("Preceeding text: ")
+                    textSize = input("Sentence amount: ")
+                    if textSize == "":
+                        textSize = 1
+                    print(model.generateAmountOfSentences(textSeed=contextString,sentNum=textSize))
                 else:
                     print("No model has been trained or loaded")
             elif inputInt == 3:
